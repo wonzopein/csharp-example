617af46e745e26c2e5e77a8dfaf2df3c6ab02d55
오승현 <seunghyeon.on@altiall.com>
2019-09-24 14:44:45 +0900

    초기 커밋.

---
+++ csharp-log4net/Common/Logger.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_log4net.Common
+{
+    public static class Logger
+    {
+        static Logger()
+        {
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("LoggerConfig.xml"));
+            Application.Info("Logger Initailized.");
+        }
+        
+        public static ILog Application { get => log4net.LogManager.GetLogger("APPLICATION"); }
+
+    }
+}
---
+++ csharp-log4net/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using csharp_log4net.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace csharp_log4net
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LogTest();
+        }
+
+        private void LogTest()
+        {
+            Logger.Application.Info("Info.");
+            Logger.Application.Debug("Debug.");
+            Logger.Application.Warn("Warn.");
+            Logger.Application.Error("Error.");
+            Logger.Application.Fatal("Fatal.");
+        }
+    }
+}
